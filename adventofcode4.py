bec7ac3a35e826e931247b139ad6f147c63d2aac
Carl Mai <carl.schoenbach@gmail.com>
2015-12-07 22:29:15 +0100

    added 4a solution

--- adventofcode4.py
+++ adventofcode4.py
@@ -4,6 +4,9 @@ a="bgvyzdsv"
 
 s = 609043
 
+for s in range(0,10000000):
+    if hashlib.md5(a+str(s)).hexdigest().startswith("00000"):
+        print s
 for s in range(1000000,10000000):
     if hashlib.md5(a+str(s)).hexdigest().startswith("000000"):
         print s
