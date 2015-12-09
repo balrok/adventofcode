8d963e7faccdc504ce4fe28058652cb226f7c4ed
Carl Mai <carl.mai@tu-dresden.de>
2015-12-09 11:19:59 +0100

    day 9

---
+++ carl/adventofcode9.py
@@ -0,0 +1,40 @@
+import itertools
+maxint = 9999999
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+
+def task1(s):
+    nodes = set()
+    edges = dict()
+    for a in s.split("\n"):
+        if a == "":
+            continue
+        x = a.split(" ")
+        nodes.add(x[0])
+        nodes.add(x[2])
+        edges[(x[0], x[2])] = int(x[4])
+        edges[(x[2], x[0])] = int(x[4])
+    #import pprint
+    #pprint.pprint(nodes)
+    #pprint.pprint(edges)
+    perm = itertools.permutations(nodes)
+    #print(len(list(perm)))
+    plength = []
+    for i in perm:
+        # getting 2-tuples (http://stackoverflow.com/questions/6007736/getting-two-tuples-out-of-a-list)
+        length = 0
+        for p in pairwise(i):
+            length += edges[tuple(p)]
+        plength.append(length)
+    print min(plength)
+    print max(plength)
+
+
+with open("input9") as f:
+    s = f.read()
+    task1(s)
