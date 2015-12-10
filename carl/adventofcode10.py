a6c2b1ddbc164be05c2c56efa3af53873c651edc
Carl Mai <carl.mai@tu-dresden.de>
2015-12-10 17:20:05 +0100

    new faster solution for day 10
    but now requires a code generator

--- carl/adventofcode10.py
+++ carl/adventofcode10.py
@@ -1,9 +1,12 @@
 import re
 import time
+import itertools
 
 start = time.time()
 s="1113222113"
-def repl(s):
+
+# re implementation
+def repl1(s):
     new = []
     # alternative solution with itertools
     # but this is slower
@@ -17,6 +20,20 @@ def repl(s):
         new.append(a[0])
     return "".join(new)
 
+
+# pure replace implementation
+def repl2(s):
+    return s.replace('111', 'h') .replace('11', 'g') .replace('1', 'f') .replace('222', 'l') .replace('22', 'k') .replace('2', 'j') .replace('333', 'p') .replace('33', 'o') .replace('3', 'n') .replace('f', '11') .replace('g', '21') .replace('h', '31') .replace('j', '12') .replace('k', '22') .replace('l', '32') .replace('n', '13') .replace('o', '23') .replace('p', '33')
+# using itertools
+def repl3(s):
+    new = []
+    f = ["".join(grp) for num, grp in itertools.groupby(s)]
+    for a in f:
+        new.append(str(len(a)))
+        new.append(a[0])
+    return "".join(new)
+
+
 for i in range(60):
-    s = repl(s)
-    print i, len(s), time.time()-start
+    s = repl2(s)
+    print i, len(s), time.time()-start #,s
