ac04dff2c7dbfcd05b7a9d1630a603da7f38da3a
Carl Mai <carl.mai@tu-dresden.de>
2015-12-10 18:34:23 +0100

    added a pure re implementation for day10 and annotated the speed

--- carl/adventofcode10.py
+++ carl/adventofcode10.py
@@ -5,7 +5,7 @@ import itertools
 start = time.time()
 s="1113222113"
 
-# re implementation
+# re implementation - medium fast
 def repl1(s):
     new = []
     # alternative solution with itertools
@@ -21,12 +21,12 @@ def repl1(s):
     return "".join(new)
 
 
-# pure replace implementation
+# pure replace implementation - fast
 def repl2(s):
     return s.replace('111', 'h') .replace('11', 'g') .replace('1', 'f') .replace('222', 'l') .replace('22', 'k') .replace('2', 'j') .replace('333', 'p') .replace('33', 'o') .replace('3', 'n') .replace('f', '11') .replace('g', '21') .replace('h', '31') .replace('j', '12') .replace('k', '22') .replace('l', '32') .replace('n', '13') .replace('o', '23') .replace('p', '33')
 
 
-# using itertools
+# using itertools - slow
 def repl3(s):
     new = []
     f = ["".join(grp) for num, grp in itertools.groupby(s)]
@@ -39,7 +39,12 @@ def repl3(s):
 def repl4(s):
     return s.replace('111', 'h') .replace('11', 'g') .replace('1', '11') .replace('222', 'l') .replace('22', 'k') .replace('2', '12') .replace('333', 'p') .replace('33', 'o') .replace('3', '13') .replace('g', '21') .replace('h', '31') .replace('k', '22') .replace('l', '32') .replace('o', '23') .replace('p', '33')
 
+# pure re implementation - slow
+def repl5f(m):
+    return str(len(m.group(0))) + m.group(1)
+def repl5(s):
+    return re.sub(r'(\d)\1*', repl5f, s)
 
 for i in range(80):
-    s = repl4(s)
+    s = repl5(s)
     print i, len(s), time.time()-start #,s
