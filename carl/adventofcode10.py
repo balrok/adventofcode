67b5acd4d4a05d55e8602173cb07c35d04c8f842
Carl Mai <carl.mai@tu-dresden.de>
2015-12-10 18:06:23 +0100

    a small micro optimization for day 10

--- carl/adventofcode10.py
+++ carl/adventofcode10.py
@@ -24,6 +24,8 @@ def repl1(s):
 # pure replace implementation
 def repl2(s):
     return s.replace('111', 'h') .replace('11', 'g') .replace('1', 'f') .replace('222', 'l') .replace('22', 'k') .replace('2', 'j') .replace('333', 'p') .replace('33', 'o') .replace('3', 'n') .replace('f', '11') .replace('g', '21') .replace('h', '31') .replace('j', '12') .replace('k', '22') .replace('l', '32') .replace('n', '13') .replace('o', '23') .replace('p', '33')
+
+
 # using itertools
 def repl3(s):
     new = []
@@ -33,7 +35,11 @@ def repl3(s):
         new.append(a[0])
     return "".join(new)
 
+# pure replace implementation with micro-optimization
+def repl4(s):
+    return s.replace('111', 'h') .replace('11', 'g') .replace('1', '11') .replace('222', 'l') .replace('22', 'k') .replace('2', '12') .replace('333', 'p') .replace('33', 'o') .replace('3', '13') .replace('g', '21') .replace('h', '31') .replace('k', '22') .replace('l', '32') .replace('o', '23') .replace('p', '33')
 
-for i in range(60):
-    s = repl2(s)
+
+for i in range(80):
+    s = repl4(s)
     print i, len(s), time.time()-start #,s
--- carl/gen10.py
+++ carl/gen10.py
@@ -1,5 +1,19 @@
 import string
 
+print "micro optimize"
+for i in range(1,4):
+    for k in range(3,0,-1):
+        if k == 1:
+            print ".replace('"+str(i)+"', '1"+str(i)+"')"
+        else:
+            print ".replace('"+(str(i)*k)+"', '"+string.ascii_lowercase[i*4+k]+"')"
+
+for i in range(1,4):
+    for k in range(2,4):
+        print ".replace('"+string.ascii_lowercase[i*4+k]+"', '"+str(k)+str(i)+"')"
+
+
+print "no optimize"
 for i in range(1,4):
     for k in range(3,0,-1):
         print ".replace('"+(str(i)*k)+"', '"+string.ascii_lowercase[i*4+k]+"')"
