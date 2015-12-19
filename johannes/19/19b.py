142b2d4fcb6479c00731739070c677f1e97a3c3a
Johannes Mey <johannes.mey@tu-dresden.de>
2015-12-19 14:47:47 +0100

    19. works with python 3 every other time... we have to discuss this

---
+++ johannes/19/19b.py
@@ -0,0 +1,76 @@
+i = """Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg"""
+
+c = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"
+
+rules = {}
+molecules = set()
+
+
+for l in i.split("\n"):
+	[v,k] = l.split(" => ")
+	if not k in rules:
+		rules[k] = []
+	rules[k].append(v)
+
+import re
+import functools
+
+
+@functools.lru_cache(maxsize=None)
+def expa(mol, it, mit):
+	if mol == "e":
+		print("found it at", it)
+		exit()
+	if it > mit:
+		return
+	for k in rules:
+		for v in rules[k]:
+			for x in re.finditer(k, mol):
+				n = mol[0:x.start()] + v + mol[x.end():]
+				expa(n, it+1, mit)
+
+for it in [213]:
+	print(it)
+	expa(c,0,it)
