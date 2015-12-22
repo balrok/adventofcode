cb62a70dda06a02225fb767861c797f01578f892
Carl Mai <carl.schoenbach@gmail.com>
2015-12-22 21:48:53 +0100

    day 22 part 2 and more comments

--- carl/adventofcode22.py
+++ carl/adventofcode22.py
@@ -29,6 +29,7 @@ def apply_timer(timer, player, boss):
         if t > 0 and spells[tid][4] == False:
             player, boss = apply_spell(spells[tid], player, boss)
             timer[tid] -= 1
+        # tmp effect spells
         if t > 0 and spells[tid][4] == True:
             if t == spells[tid][3]:
                 player, boss = apply_spell(spells[tid], player, boss)
@@ -38,19 +39,30 @@ def apply_timer(timer, player, boss):
             # remove tmp effect
             if timer[tid] == 0:
                 player, boss = remove_spell(spells[tid], player, boss)
-
     return timer, player, boss
 
 
-def fight(base_player, base_boss, base_timer, turn=1, used_mana=0, fewest_mana = 13560):
+def fight(base_player, base_boss, base_timer, turn=1, used_mana=0, fewest_mana = 99999):
+    # without this optimization 9.6 seconds - with this 7.4
+    # so not so important
+    # I tested: my max turn amount is 19
+    # so just len(spells)*19 different routes I think?
     if used_mana > fewest_mana:
         return False, fewest_mana
+    # I need to copy everything to _0 to apply the timer before the spell loop
     player0 = base_player[:]
     boss0 = base_boss[:]
     timer0 = base_timer[:]
+    # apply the timer
     timer0, player0, boss0 = apply_timer(timer0, player0, boss0)
+    if task2:
+        player0[0] -= 1
+        if player0[0] <= 0:
+            return False, used_mana
     if boss0[0] <= 0:
         return True, used_mana
+
+    # now loop through the spells (sorted by mana-amount to break the loop when not enough)
     won = False
     for sid in range(len(spells)):
         #print turn, sid
@@ -67,25 +79,29 @@ def fight(base_player, base_boss, base_timer, turn=1, used_mana=0, fewest_mana =
             #print "mana", player
             break
         # only those with instant effect now
-        #print turn, "cast", s[0]
-        player[3] -= s[0]
-        if s[3] == 0:
+        # print turn, "cast", s[0]
+        player[3] -= s[0] # mana
+        if s[3] == 0: # this is no timer-spell
             player, boss = apply_spell(s, player, boss)
-        if s[3] > 0:
+        else:
             timer[sid] = s[3]
+
+        # Boss turn
+        timer, player, boss = apply_timer(timer, player, boss)
         if boss[0] <= 0:
             return True, used_mana+s[0]
-
-        timer, player, boss = apply_timer(timer, player, boss)
         player[0] -= boss[1]-player[2]
         #print turn, player, boss, timer
         if player[0] <= 0:
             #print "player dead"
             return False, fewest_mana
         #print turn, player, boss
+
+        # take this spell and go to the next turn
         result, mana = fight(player, boss, timer, turn+1, used_mana+s[0], fewest_mana)
         if result:
             if mana < fewest_mana:
+                # with this I can follow back which spells were cast on the best route
                 print turn, mana, player, boss, s[0], timer
             fewest_mana = min(mana, fewest_mana)
             won = True
@@ -96,10 +112,16 @@ def fight(base_player, base_boss, base_timer, turn=1, used_mana=0, fewest_mana =
     
 
 
-# 1161 too low
+
+task2 = False
 timer = len(spells) * [0]
 print fight(list(player[:]), list(boss[:]), timer)
 
+print "\n\n"
+
+task2 = True
+timer = len(spells) * [0]
+print fight(list(player[:]), list(boss[:]), timer)
 # this is a test
 #player = (10, 0, 0, 250)
 #boss = (13, 8, 0, 0)
